b0b7014cbf4bae3a68ae7457d4699982d71c8b4c
Omoju Miller <omoju@github.com>
2020-07-27 21:15:24 +0000

    update

--- src/main/java/com/github/demo/service/BookService.java
+++ src/main/java/com/github/demo/service/BookService.java
@@ -10,12 +10,12 @@ public class BookService {
     private static List<Book> books = new ArrayList<Book>(5);
 
     static {
-        books.add(new Book("Jeff Sutherland","Scrum: The Art of Doing Twice the Work in Half the Time", "scrum.jpg"));
-        books.add(new Book("Eric Ries","The Lean Startup: How Constant Innovation Creates Radically Successful Businesses", "lean.jpg"));
-        books.add(new Book("Geoffrey A. Moore","Crossing the Chasm", "chasm.jpg"));
-        books.add(new Book("David Thomas","The Pragmatic Programmer: From Journeyman to Master", "pragmatic.jpg"));
-        books.add(new Book("Frederick P. Brooks Jr.", "The Mythical Man-Month: Essays on Software Engineering", "month.jpg"));
-        books.add(new Book("Steve Kruggieman","Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability", "think.jpg"));
+        books.add(new Book("Jeff Sutherland","Scrum: The Art of Doing Twice the Work in Half the Time", "scrum.jpg", 3));
+        books.add(new Book("Eric Ries","The Lean Startup: How Constant Innovation Creates Radically Successful Businesses", "lean.jpg", 5));
+        books.add(new Book("Geoffrey A. Moore","Crossing the Chasm", "chasm.jpg", 4));
+        books.add(new Book("Steve Krug","Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability", "think.jpg", 5));
+        books.add(new Book("Isabel Wilkerson","The Warmth of Other Suns: The Epic Story of America's Great Migration", "Wilkerson.jpg", 5));
+        //books.add(new Book("Nadia Egbhal", "Working in Public: The Making and Maintenance of Open Source Software", "Egbhal_OS.jpg");
     }
 
     public List<Book> getBooks() {
